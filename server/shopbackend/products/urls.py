9b12e560de1074342f819b82044013326657c612
Volodya <proger.sdk@gmail.com>
2020-05-30 11:52:35 +0300

    Server: Some updates

--- server/shopbackend/products/urls.py
+++ server/shopbackend/products/urls.py
@@ -13,10 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import include, path
+from rest_framework import routers
+from . import views
+
+router = routers.DefaultRouter()
+router.register('products', views.ProductViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('products.urls')),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
