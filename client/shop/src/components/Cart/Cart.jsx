4fceb8455998289f5c85b73a95a18964fecca5ea
Volodya <proger.sdk@gmail.com>
2020-05-31 18:11:45 +0300

    Client: Add Cart template

--- client/shop/src/components/Cart/Cart.jsx
+++ client/shop/src/components/Cart/Cart.jsx
@@ -13,6 +13,58 @@ const Cart = (props) => {
           Продовжуйте перегляд <Link to={path.products}>тут</Link>.
         </p>
       </div>
+      <div className={classes.filledCart}>
+        <div className={classes.products}>
+          <div className={classes.productsHeader}>
+            <span className={classes.productH}>Продукт</span>
+            <span className={classes.priceH}>Ціна</span>
+            <span className={classes.quantityH}>Кількість</span>
+            <span className={classes.totalH}>Загалом</span>
+          </div>
+          <div className={classes.productsContainer}>
+            <CartItem />
+            <CartItem />
+            <CartItem />
+          </div>
+        </div>
+        <div className={classes.cartFooter}>
+          <div className={classes.comment}>
+            <div>
+              <label name='comment'>Спеціальні інструкції для продавця</label>
+            </div>
+            <div>
+              <textarea name='comment'></textarea>
+            </div>
+          </div>
+          <div className={classes.bill}>
+            <div className={classes.totalPrice}>Загалом ₴1599.95</div>
+            <div className={classes.buttons}>
+              <Link to={path.products} className={classes.btnContinue}>Продовжити покупки</Link>
+              <Link to={path.checkOut}className={classes.btnCheckOut}>Check Out</Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const CartItem = () => {
+  let imgLink = 'https://freesvg.org/img/Placeholder.png'
+
+  return (
+    <div className={classes.cartItem}>
+      <div className={classes.itemImage}>
+        <img src={imgLink} alt='' />
+      </div>
+      <div className={classes.itemDescription}>
+        <h4>Stealth Bomber Jacket - Navy</h4>
+        <div>Small</div>
+        <div>Видалити</div>
+      </div>
+      <div className={classes.itemPrice}>$1,599.95</div>
+      <div className={classes.itemQuantity}>1</div>
+      <div className={classes.itemTotal}>$1,599.95</div>
     </div>
   )
 }
