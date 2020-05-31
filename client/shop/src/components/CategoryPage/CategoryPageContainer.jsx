139199f5fd8f51da0be1d0fd029795b4c2933394
Volodya <proger.sdk@gmail.com>
2020-05-31 19:11:38 +0300

    Client: Minor changes

--- client/shop/src/components/CategoryPage/CategoryPageContainer.jsx
+++ client/shop/src/components/CategoryPage/CategoryPageContainer.jsx
@@ -3,35 +3,26 @@ import CategoryPage from './CategoryPage'
 import * as axios from 'axios'
 
 class CategoryPageContainer extends React.Component {
-  
   constructor(props) {
     super(props)
     this.api_url = 'http://127.0.0.1:8000/products/'
-    this.state = {data: []}
+    this.state = { data: [] }
 
-    if(this.props.category) {
-      this.api_url =  this.api_url + 'category/' + this.props.category + '/'
+    if (this.props.category) {
+      this.api_url = this.api_url + 'category/' + this.props.category + '/'
     }
   }
 
-  // category = this.props.category
   componentDidMount() {
-    axios.get(this.api_url)
-      .then((response) => {
-        this.setState({
-          data: response.data
+    axios.get(this.api_url).then((response) => {
+      this.setState({
+        data: response.data,
       })
-      console.log(response)
+      // console.log(response)
     })
-    
   }
-  
-  
-  
+
   render() {
-    // if(this.state.data[0])
-    //   console.log(this.state.data[0].name)
-    // debugger
     return <CategoryPage title={this.props.title} state={this.state.data} />
   }
 }
--- client/shop/src/components/common/ProductPage/ProductPageContainer.jsx
+++ client/shop/src/components/common/ProductPage/ProductPageContainer.jsx
@@ -5,40 +5,27 @@ import { connect } from 'react-redux'
 import * as axios from 'axios'
 
 class ProductPageContainer extends React.Component {
-
   constructor(props) {
     super(props)
-    
-    // this.category = this.props.match.params.category
     this.productId = this.props.match.params.productId
     console.log(this.productId)
 
-    this.api_url = 'http://127.0.0.1:8000/products/product/' + this.productId + '/'
-    this.state = {data: {}}
+    this.api_url =
+      'http://127.0.0.1:8000/products/product/' + this.productId + '/'
+    this.state = { data: {} }
   }
 
-  // category = this.props.category
   componentDidMount() {
-    axios.get(this.api_url)
-      .then((response) => {
-        this.setState({
-          data: response.data
+    axios.get(this.api_url).then((response) => {
+      this.setState({
+        data: response.data,
       })
-      console.log(response)
+      // console.log(response)
     })
-
   }
 
   render() {
-    // debugger
-    // let category = this.props.match.params.category
-    // let productId = this.props.match.params.productId
-
-    // let product = this.props.collections[category].find(
-    //   (element) => element.id === productId
-    // )
     return <ProductPage product={this.state.data} />
-    // return <></>
   }
 }
 
