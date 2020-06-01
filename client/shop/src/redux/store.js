58903810848c2972e15643e5cb05d1160b229281
Volodya <proger.sdk@gmail.com>
2020-06-01 12:34:39 +0300

    Client: Delete collections

--- client/shop/src/redux/store.js
+++ client/shop/src/redux/store.js
@@ -1,10 +1,8 @@
 import { createStore, combineReducers } from 'redux'
-import collectionsReducer from './collections-reducer'
 import cartReducer from './cart-reducer'
 
 let reducers = combineReducers({
-  collections: collectionsReducer,
-  cartPage: cartReducer,
+  cart: cartReducer,
 })
 
 let store = createStore(reducers)
